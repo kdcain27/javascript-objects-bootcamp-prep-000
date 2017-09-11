eb6863d438cc9bf263d54af8dfbde37b1b2c7020
Kevin Krause <kkrausesc@gmail.com>
2017-09-11 12:38:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,8 +4,3 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
   return playlist
 }
-
-function removeFromPlaylist(playlist,artistName) {
-  delete playlist[artistName] = songTitle
-  return playlist
-}

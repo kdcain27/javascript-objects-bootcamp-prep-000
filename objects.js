1d8a9abc82402ece7c4aba9e84caf9b99a9a8e4a
Kevin Krause <kkrausesc@gmail.com>
2017-09-11 12:33:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistNme: 'songTitle'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = 'songTitle'
+  playlist[artistName] = songTitle
   return playlist
 }

b201ce105d4d8e05fb62933a9dedbc974eab33d6
Kevin Krause <kkrausesc@gmail.com>
2017-09-11 12:24:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,3 @@
 var playlist = {}
 
 Object.assign({}, playlist, { artistName: 'songTitle' })
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({}, playlist, { artistName: 'songTitle' })
-}return playlist;
